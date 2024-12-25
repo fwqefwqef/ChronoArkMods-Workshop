80bbff7b91bbbf4626df484df07726b4ce08d70a
SJ Kim <46618311+fwqefwqef@users.noreply.github.com>
2024-12-25 19:36:20 +0900

    Add files via upload

---
+++ Item_Equip/Mistletoe_2.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using GameDataEditor;
+using I2.Loc;
+using DarkTonic.MasterAudio;
+using ChronoArkMod;
+using ChronoArkMod.Plugin;
+using ChronoArkMod.Template;
+using Debug = UnityEngine.Debug;
+namespace NightmareEve
+{
+	/// <summary>
+	/// Mistletoe
+	/// Gains stats as you defeat bosses. (Must be equipped)
+	/// Enduring Soul: Once per stage, endure a fatal attack and heal HP to 1.
+	/// Enduring Soul: &a
+	/// </summary>
+    public class Mistletoe_2:EquipBase
+    {
+
+    }
+}

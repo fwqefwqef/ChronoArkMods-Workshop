63d977670bfdffed3c23f3388e029a17767925a1
SJ Kim <46618311+fwqefwqef@users.noreply.github.com>
2024-12-25 19:38:50 +0900

    Add files via upload

---
+++ NightmareEve/AI.cs
@@ -0,0 +1,288 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NightmareEve
+{
+	using System;
+
+	public class AI_Abbadon : AI
+	{
+
+		public override int SpeedChange(Skill skill, int ActionCount, int OriginSpeed)
+		{
+			if (skill.MySkill.KeyID == "S_Abbadon_1")
+			{
+				return 1;
+			}
+			else
+            {
+				return base.SpeedChange(skill, ActionCount, OriginSpeed);
+            }
+		}
+
+		public override Skill SkillSelect(int ActionCount)
+		{
+			if (ActionCount == 0)
+			{
+				return this.BChar.Skills[0];
+			}
+			return this.BChar.Skills[1];
+		}
+	}
+
+	public class AI_Matador : AI
+	{
+		public override Skill SkillSelect(int ActionCount)
+		{
+			// Odd
+			if (BattleSystem.instance.TurnNum % 2 == 1)
+			{
+				if (ActionCount == 0)
+				{
+					return this.BChar.Skills[0];
+				}
+				if (ActionCount == 1)
+				{
+					return this.BChar.Skills[1];
+				}
+				if (ActionCount == 2)
+				{
+					return this.BChar.Skills[2];
+				}
+			}
+			// Even
+			if (BattleSystem.instance.TurnNum % 2 == 0)
+			{
+				if (ActionCount == 0)
+				{
+					return this.BChar.Skills[3];
+				}
+				if (ActionCount == 1)
+				{
+					return this.BChar.Skills[4];
+				}
+				if (ActionCount == 2)
+				{
+					return this.BChar.Skills[4];
+				}
+			}
+			return this.BChar.Skills[0];
+		}
+	}
+
+	public class AI_WhiteRider : AI
+	{
+		public override int SpeedChange(Skill skill, int ActionCount, int OriginSpeed)
+		{
+			if (skill.MySkill.KeyID == "S_WhiteRider_1")
+			{
+				return 0;
+			}
+			else
+			{
+				return base.SpeedChange(skill, ActionCount, OriginSpeed);
+			}
+		}
+
+		public override Skill SkillSelect(int ActionCount)
+		{
+			// Odd
+			if (BattleSystem.instance.TurnNum % 4 == 1)
+			{
+				return this.BChar.Skills[1];
+			}
+			return this.BChar.Skills[0];
+		}
+	}
+	public class AI_RedRider : AI
+	{
+		public override int SpeedChange(Skill skill, int ActionCount, int OriginSpeed)
+		{
+			if (skill.MySkill.KeyID == "S_RedRider_1")
+			{
+				return 0;
+			}
+			else
+			{
+				return base.SpeedChange(skill, ActionCount, OriginSpeed);
+			}
+		}
+		public override Skill SkillSelect(int ActionCount)
+		{
+			// Odd
+			if (BattleSystem.instance.TurnNum % 4 == 2)
+			{
+				return this.BChar.Skills[1];
+			}
+			return this.BChar.Skills[0];
+		}
+	}
+	public class AI_BlackRider : AI
+	{
+		public override Skill SkillSelect(int ActionCount)
+		{
+			// Odd
+			if (BattleSystem.instance.TurnNum % 4 == 3)
+			{
+				return this.BChar.Skills[1];
+			}
+			return this.BChar.Skills[0];
+		}
+	}
+	public class AI_PaleRider : AI
+	{
+		public override Skill SkillSelect(int ActionCount)
+		{
+			if (ActionCount == 0)
+            {
+				if (BattleSystem.instance.TurnNum % 4 == 0)
+				{
+					return this.BChar.Skills[2];
+				}
+				else
+                {
+					return this.BChar.Skills[1]; // Mamudoon
+				}
+			}
+
+			else
+            {
+				return this.BChar.Skills[0]; // Dark Might
+			}
+		}
+	}
+	public class AI_Belial : AI
+    {
+		public bool usedFlame = false;
+        public override Skill SkillSelect(int ActionCount)
+		{
+			if (BattleSystem.instance.TurnNum == 1 && ActionCount == 0)
+			{
+				return this.BChar.Skills[0];
+			}
+			else if (Misc.NumToPer((float)this.BChar.GetStat.maxhp, (float)this.BChar.HP) <= 50f && !usedFlame)
+            {
+				if (ActionCount == 0)
+                {
+					BattleSystem.instance.AllyTeam.Add(Skill.TempSkill("S_Sacrifice", BattleSystem.instance.AllyTeam.LucyChar, BattleSystem.instance.AllyTeam), true);
+					return this.BChar.Skills[5];
+				}
+				else
+				{
+					if (ActionCount == 2)
+					{
+						usedFlame = true;
+					}
+					return null;
+				}
+            }
+			return base.SkillRandomSelect(new List<Skill>{this.BChar.Skills[1], this.BChar.Skills[2], this.BChar.Skills[3], this.BChar.Skills[4]});
+		}
+	}
+
+	public class AI_Beelzebub : AI
+	{
+		public override int SpeedChange(Skill skill, int ActionCount, int OriginSpeed)
+		{
+			if (ActionCount == 0)
+			{
+				return 1;
+			}
+			else
+			{
+				return base.SpeedChange(skill, ActionCount, OriginSpeed);
+			}
+		}
+
+		public bool usedDeath = false;
+		public override Skill SkillSelect(int ActionCount)
+		{
+			if (Misc.NumToPer((float)this.BChar.GetStat.maxhp, (float)this.BChar.HP) <= 50f && !usedDeath)
+			{
+				if (ActionCount == 0)
+				{
+					return this.BChar.Skills[7];
+				}
+				else
+				{
+					if (ActionCount == 2)
+					{
+						usedDeath = true;
+					}
+					return null;
+				}
+			}
+			else if (ActionCount == 0)
+            {
+				return base.SkillRandomSelect(new List<Skill> { this.BChar.Skills[0], this.BChar.Skills[1], this.BChar.Skills[2]});
+			}
+			else if (ActionCount == 2 && BattleSystem.instance.TurnNum%2==0)
+            {
+				return this.BChar.Skills[3];
+			}
+			return base.SkillRandomSelect(new List<Skill> { this.BChar.Skills[4], this.BChar.Skills[5], this.BChar.Skills[6]});
+		}
+	}
+
+	public class AI_Angel : AI
+	{
+		public override int SpeedChange(Skill skill, int ActionCount, int OriginSpeed)
+		{
+			if (skill.MySkill.KeyID == "S_RedRider_1" || skill.MySkill.KeyID == "S_WhiteRider_1")
+			{
+				return 0;
+			}
+			else
+			{
+				return base.SpeedChange(skill, ActionCount, OriginSpeed);
+			}
+		}
+
+		public override Skill SkillSelect(int ActionCount)
+		{
+			if (BattleSystem.instance.TurnNum%2==1)
+            {
+				return this.BChar.Skills[0];
+			}
+			return this.BChar.Skills[1];
+		}
+	}
+
+	public class AI_Metatron : AI
+	{
+
+		public override Skill SkillSelect(int ActionCount)
+		{
+			// Phase change
+			if (this.BChar.HP <= 1000)
+            {
+                //if (ActionCount == 2)
+                //{
+                //    return null;
+                //}
+                return this.BChar.Skills[0];
+			}
+
+			if (ActionCount == 0)
+            {
+				if (BattleSystem.instance.TurnNum % 2 == 1)
+				{
+					return this.BChar.Skills[0]; // Meg
+				}
+				if (BattleSystem.instance.TurnNum % 2 == 0)
+				{
+					if (BattleSystem.instance.EnemyTeam.AliveChars.Count == 5)
+                    {
+						return this.BChar.Skills[0]; // Meg
+					}
+					return this.BChar.Skills[6]; // Recarm
+				}
+			}
+
+			return base.SkillRandomSelect(new List<Skill> { this.BChar.Skills[1], this.BChar.Skills[2], this.BChar.Skills[3], this.BChar.Skills[4], this.BChar.Skills[5] });
+		}
+	}
+
+}
---
+++ NightmareEve/NightmareEve_ModDefinition.cs
@@ -0,0 +1,62 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using GameDataEditor;
+using I2.Loc;
+using DarkTonic.MasterAudio;
+using ChronoArkMod;
+using ChronoArkMod.Plugin;
+using ChronoArkMod.Template;
+using Debug = UnityEngine.Debug;
+using ChronoArkMod.ModData;
+namespace NightmareEve
+{
+    public class NightmareEve_ModDefinition:ModDefinition
+    {
+        public override Type ModItemKeysType => typeof(ModItemKeys);
+        /* //Example
+        [CustomGDE(nameof(GDEItemKeys.Character_Sizz),nameof(GDECharacterData.name))]
+        public string SizzName
+        {
+            get
+            {
+                return $"Sizz(New Name of {ModId})";
+            }
+        }
+        [CustomGDE(nameof(GDEItemKeys.Skill_S_Sizz_2), "UseAp")]
+        [CustomGDE(nameof(GDEItemKeys.Skill_S_Sizz_1), "UseAp")]
+        public static int Sizz1AP(int oldap)
+        {
+            return oldap + 1;
+        }
+        [CustomGDE("S_Sizz_1", nameof(GDESkillData.Except))]
+        public static bool Sizz1Except = true;
+        */
+    }
+    /* //Example
+    public class ExampleSkill : CustomSkillGDE<NightmareEve_ModDefinition>
+    {
+        public override string Key()
+        {
+            return "ExampleSkill"; //it will override your "ExampleSkill" gdata in the mod editor
+        }
+        public override ModGDEInfo.LoadingType GetLoadingType()
+        {
+            return ModGDEInfo.LoadingType.Add; 
+        }
+        public override void SetValue()
+        {
+            PlusSkillView = ModKey<ExampleSkill>();//for ModDefinition gdata
+            User = GDEItemKeys.Character_Azar;//for gdata of orginial game
+            SkillExtended = new List<string> { typeof(ExampleSkill_SkillExtended).AssemblyQualifiedName };//for script
+            //Image_0 = assetInfo.ImageFromAsset("Your AssetBundle Path", "Path in Unity"); 
+        }
+        public class ExampleSkill_SkillExtended:Skill_Extended
+        {
+
+        }
+    }*/
+}
